7a1a344fe30fc3cef472383592aabf0820296dc0
Lyx71338427 <2114536821@qq.com>
2026-03-03 16:40:39 +0800

    Initial Maven project + MVC skeleton

---
+++ src/main/java/org/sudoku/app/CLIMain.java
@@ -0,0 +1,5 @@
+package org.sudoku.app;
+
+public class CLIMain {
+    public static void main(String[] args) {}
+}
---
+++ src/main/java/org/sudoku/app/GUIMain.java
@@ -0,0 +1,5 @@
+package org.sudoku.app;
+
+public class GUIMain {
+    public static void main(String[] args) {}
+}
