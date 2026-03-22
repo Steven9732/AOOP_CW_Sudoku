ffb5dfb96d6d09aaa2f76aa683b567a7190c19ac
Lyx71338427 <2114536821@qq.com>
2026-03-22 18:40:47 +0800

    feat(controller): implemented erase action for clear cell value in controller

---
+++ src/main/java/org/sudoku/controller/EraseAction.java
@@ -0,0 +1,27 @@
+package org.sudoku.controller;
+
+import org.sudoku.model.Model;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.function.IntSupplier;
+
+public class EraseAction implements ActionListener{
+    private Model model;
+    private IntSupplier selectedRow;
+    private IntSupplier selectedColumn;
+
+    public EraseAction(Model model, IntSupplier selectedRow, IntSupplier selectedColumn) {
+        this.model = model;
+        this.selectedRow = selectedRow;
+        this.selectedColumn = selectedColumn;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        int row = selectedRow.getAsInt();
+        int column = selectedColumn.getAsInt();
+        if (row < 0 || column < 0) return;
+        model.clearValue(row, column);
+    }
+}
