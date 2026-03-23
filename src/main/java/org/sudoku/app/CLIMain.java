53b5d1c6764c2a38677973bc376c98b086d643b9
Lyx71338427 <2114536821@qq.com>
2026-03-23 13:38:56 +0800

    feat(cli): added game board printing in CLIMain

--- src/main/java/org/sudoku/app/CLIMain.java
+++ src/main/java/org/sudoku/app/CLIMain.java
@@ -1,5 +1,29 @@
 package org.sudoku.app;
 
+import org.sudoku.model.Model;
+
 public class CLIMain {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        Model model = new Model();
+
+        System.out.println("This is a Sudoku Game");
+        System.out.println("Type 'help' to see commands.");
+
+        printBoard(model);
+
+    }
+
+    // Print game board
+    private static void printBoard (Model model) {
+        for (int row = 0; row < model.SIZE; row++) {
+            if (row % 3 == 0) System.out.println("+------+------+------+");
+            for (int col = 0; col < model.SIZE; col++) {
+                if (col % 3 == 0) System.out.print("|");
+                int value = model.getCellValue(row, col);
+                System.out.print(value == 0 ? ". " : (value + " "));
+            }
+            System.out.println("|");
+        }
+        System.out.println("+------+------+------+");
+    }
 }
