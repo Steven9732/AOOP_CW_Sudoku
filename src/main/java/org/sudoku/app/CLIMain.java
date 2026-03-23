ffa61adcdcd90a3de1d705e0d422c61cb2aa4237
Lyx71338427 <2114536821@qq.com>
2026-03-23 15:34:46 +0800

    feat(cli): implemented function of showing all necessary commands in cli

--- src/main/java/org/sudoku/app/CLIMain.java
+++ src/main/java/org/sudoku/app/CLIMain.java
@@ -2,14 +2,39 @@ package org.sudoku.app;
 
 import org.sudoku.model.Model;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 public class CLIMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Model model = new Model();
 
         System.out.println("This is a Sudoku Game");
         System.out.println("Type 'help' to see commands.");
 
         printBoard(model);
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            System.out.print("> ");
+            String line = br.readLine();
+            if (line == null) break;
+
+            line = line.trim();
+            if (line.isEmpty()) continue;
+            String[] token = line.split("\\s+");
+            String command = token[0].toLowerCase();
+
+            try {
+                boolean stateChanged = false;
+                switch (command) {
+                    case "help" -> printCommands();
+                }
+            } finally {
+
+            }
+        }
 
     }
 
@@ -25,5 +50,19 @@ public class CLIMain {
             System.out.println("|");
         }
         System.out.println("+------+------+------+");
+    }
+
+    private static void printCommands () {
+            System.out.println("""
+                Commands:
+                  show
+                  set row column value        (row, column, value are 1..9)
+                  clear row column        (or: erase row column)
+                  undo
+                  hint
+                  reset
+                  new              (or: newgame)
+                  quit
+                """);
     }
 }
