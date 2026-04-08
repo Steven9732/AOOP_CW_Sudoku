44404ca081cb58f2d72320659f8c03709b23d1db
Lyx71338427 <2114536821@qq.com>
2026-04-08 15:19:00 +0800

    feat(gui): added gui for sudoku game to play

--- src/main/java/org/sudoku/app/GUIMain.java
+++ src/main/java/org/sudoku/app/GUIMain.java
@@ -1,5 +1,20 @@
 package org.sudoku.app;
 
+import org.sudoku.model.Model;
+import org.sudoku.controller.SudokuController;
+import org.sudoku.view.SudokuFrame;
+
+import javax.swing.SwingUtilities;
+
 public class GUIMain {
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            Model model = new Model();
+            SudokuFrame frame = new SudokuFrame();
+            SudokuController controller = new SudokuController(model, frame);
+
+            frame.setVisible(true);
+            frame.requestFocusInWindow();
+        });
+    }
 }
